bbeafc57c5fa835222e32bd2faeee6a492d656ab
Kitty Sinaga <kitty.sinaga@gmail.com>
2024-04-29 21:28:46 -0400

    fruit sales assignment

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,4 @@
-# add your code here
+import pandas as pd
+
+fruit = pd.DataFrame({"Apples":[35,41], "Bananas":[21,34]}, index=["2017 Sales", "2018 Sales"])
+fruit.to_csv("fruit.csv",sep=",",index=False)
